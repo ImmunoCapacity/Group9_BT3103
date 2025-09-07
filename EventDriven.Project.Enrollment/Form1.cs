c2b8e0f144ff147456287dc2c66bfd95a15b1c34
Michael Tagle <132924050+ImmunoCapacity@users.noreply.github.com>
2025-09-07 23:37:09 +0800

    Revert "Login secured"

---
+++ EventDriven.Project.Enrollment/Form1.cs
@@ -0,0 +1,10 @@
+namespace EventDriven.Project.Enrollment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- EventDriven.Project.UI/Program.cs
+++ EventDriven.Project.UI/Program.cs
@@ -10,20 +10,8 @@ namespace EventDriven.Project.UI
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-
             ApplicationConfiguration.Initialize();
-            using (TempLogin loginForm = new TempLogin())
-            {
-                if (loginForm.ShowDialog() == DialogResult.OK)
-                {
-                    Application.Run(new Form1());
-                }
-                else
-                {
-                    Application.Exit();
-
-                }
-            }
+            Application.Run(new Form1());
         }
     }
 }
