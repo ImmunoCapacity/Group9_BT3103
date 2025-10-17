2b2f345bf3626e05946c7c96128820ff7193dba2
Tagle <Tagle@MICHAEL>
2025-10-18 00:08:58 +0800

    Database for Schedule and Payment

---
+++ EventDriven.Project.DB/dbo/Tables/Academic Information Table/tblSubjects.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblSubjects]
+(
+	[SubjectID] INT PRIMARY KEY,
+	[SubjectName] NVARCHAR(100) NOT NULL,
+	[Category] NVARCHAR(100) NULL
+)
---
+++ EventDriven.Project.DB/dbo/Tables/Academic Information Table/tblTeachers.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[tblTeachers]
+(
+	[TeacherID] INT IDENTITY(100,1) PRIMARY KEY,
+	[TeacherName] NVARCHAR(150) NOT NULL
+)
