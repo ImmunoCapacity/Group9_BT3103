76c1f9c293172c27d5aa4b7df17ad7d09c016a7f
Demonastis <kim.player.unknown@gmail.com>
2025-10-22 15:58:45 +0800

    test

---
+++ EventDriven.Project.Model/RegistrationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDriven.Project.Model
+{
+    public class RegistrationModel
+    {
+        public  int Id { get; set; }
+        public string Section { get; set; }
+        public string Requirements { get; set; }
+        public 
+    }
+}
