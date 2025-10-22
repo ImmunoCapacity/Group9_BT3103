42280ce3e6e32356166c1a9e7cbb7e4d07c0477a
Demonastis <kim.player.unknown@gmail.com>
2025-10-22 17:04:23 +0800

    andrei model edits

---
+++ EventDriven.Project.Model/FullPaymentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDriven.Project.Model
+{
+    public class FullPaymentModel
+    {
+        public int Id { get; set; }
+        public string AmountPaid {  get; set; }
+        public string DateTime { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
---
+++ EventDriven.Project.Model/SubjectModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDriven.Project.Model
+{
+    public class SubjectModel
+    {
+        public int SubjectID { get; set; }
+        public string SubjectName { get; set; }
+        public string Category { get; set; }
+    }
+}
